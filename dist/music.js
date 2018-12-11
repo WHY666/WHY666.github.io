e0e296fe9a1a589a8a126f969e7f8c30c50085f2
WHY666 <root@vultr.guest>
2018-12-11 09:40:20 +0000

    Site updated: 2018-12-11 09:40:15

--- dist/music.js
+++ dist/music.js
@@ -4,59 +4,151 @@ const ap = new APlayer({
     autoplay: true,
     mini: true,
     audio: [
- {
+   {
+        name: "夏天的风",
+        artist: '温岚',
+        url: '/dist/songs/夏天的风',
+        cover: 'https://imgessl.kugou.com/stdmusic/20160907/20160907221352180114.jpg',
+      },  
+    {
+        name: "笔记本",
+        artist: '陈慧琳',
+        url: '/dist/songs/笔记本',
+        cover: 'https://imgessl.kugou.com/stdmusic/20160907/20160907201549230205.jpg',
+    },      
+   
+    {
+        name: "笔记",
+        artist: '周笔畅',
+        url: '/dist/songs/笔记',
+        cover: 'https://imgessl.kugou.com/stdmusic/20180117/20180117181020178380.jpg',
+    },            
+
+    {
+        name: "melody",
+        artist: '陶喆',
+        url: '/dist/songs/melody',
+        cover: 'https://imgessl.kugou.com/stdmusic/20150718/20150718174252663587.jpg',
+    },    
+   
+        
+    {
+        name: "真的爱你",
+        artist: 'Beyond',
+        url: '/dist/songs/真的爱你',
+        cover: 'https://imgessl.kugou.com/stdmusic/20150715/20150715232800432202.jpg',
+    },     
+    
+    {
+        name: "Ce Frumoasa E Iubirea",
+        artist: 'Giulia',
+        url: '/dist/songs/Ce Frumoasa E Iubirea.mp3',
+        cover: 'https://imgessl.kugou.com/stdmusic/20150718/20150718085511381828.jpg',
+    },  
+    
+    {
+        name: "小宇",
+        artist: '张震岳',
+        url: '/dist/songs/小宇.mp3',
+        cover: 'https://imgessl.kugou.com/uploadpic/softhead/400/20180906/20180906191632536.jpg',
+    },  
+    
+    {
+        name: "遇见",
+        artist: '孙燕姿',
+        url: '/dist/songs/遇见.mp3',
+        cover: 'https://imgessl.kugou.com/stdmusic/20150714/20150714164620754955.jpg',
+    },  
+    
+    {
+        name: "大城小爱",
+        artist: '王力宏',
+        url: '/dist/songs/大城小爱.mp3',
+        cover: 'https://imgessl.kugou.com/stdmusic/20150228/20150228143443426128.jpg',
+    },  
+    
+    {
+        name: "一生有你",
+        artist: '水木年华',
+        url: '/dist/songs/一生有你.mp3',
+        cover: 'https://imgessl.kugou.com/stdmusic/20150716/20150716023319909457.jpg',
+    },     
+    
+    {
+        name: "岁月神偷",
+        artist: '金玟岐',
+        url: '/dist/songs/岁月神偷.mp3',
+        cover: 'https://imgessl.kugou.com/stdmusic/20160907/20160907235233141820.jpg',
+    }, 
+    
+    {
+        name: "画沙",
+        artist: '周杰伦',
+        url: '/dist/songs/画沙.mp3',
+        cover: 'https://imgessl.kugou.com/stdmusic/20160623/20160623233610830051.jpg',
+    },         
+    
+    {
         name: "平凡之路--木吉他版",
         artist: '朴树',
         url: '/dist/songs/平凡之路木吉他.mp3',
-        cover: 'https://imgessl.kugou.com/stdmusic/20160907/20160907184638429200.jpg',
-      },     
+        cover: 'https://p2.music.126.net/etEzXkRd2FXo7IqrlOPC4Q==/16601526067929229.jpg?param=34y34',
+    },     
+    
     {
         name: "我们的歌",
         artist: '王力宏',
         url: '/dist/songs/我们的歌.mp3',
         cover: 'https://imgessl.kugou.com/stdmusic/20160907/20160907184638429200.jpg',
-      }, 
-      {
+    }, 
+    
+    {
         name: '单恋高校',
         artist: '陈冠希',
         url: '/dist/songs/单恋高校.mp3',
         cover: 'https://imgessl.kugou.com/stdmusic/20160907/20160907173524224717.jpg',
-      },           
-      {
+    },           
+    
+    {
         name: "China-X",
         artist: '徐梦圆',
         url: '/dist/songs/China-X.mp3',
         cover: 'https://imgessl.kugou.com/stdmusic/20160812/20160812190229390849.jpg',
-      },    
-      {
+    },    
+    
+    {
         name: '9420',
         artist: '麦小兜',
         url: '/dist/songs/9420.mp3',
         cover: 'https://imgessl.kugou.com/stdmusic/20171025/20171025101931506152.jpg',
-      },
-      {
+    },
+    
+    {
         name: '最美的期待',
         artist: '周笔畅',
         url: '/dist/songs/最美的期待.mp3',
         cover: 'https://imgessl.kugou.com/stdmusic/20180117/20180117181020178380.jpg',
-      },
-      {
+    },
+    
+    {
         name: '北京东路的日子',
         artist: '刘千楚',
         url: '/dist/songs/北京东路的日子.mp3',
         cover: 'https://p1.music.126.net/nioPMqyc0opzEavs67vdYw==/19128203788658830.jpg',
-      },     
-      {
+    },     
+    
+    {
         name: '她说',
         artist: '林俊杰',
         url: '/dist/songs/她说.mp3',
         cover: 'https://imgessl.kugou.com/stdmusic/20170424/20170424173313557089.jpg',
-      },
-      {
+    },
+      
+    {
         name: '海阔天空',
         artist: 'BEYOND',
         url: '/dist/songs/海阔天空.mp3',
         cover: 'https://imgessl.kugou.com/stdmusic/20150715/20150715232800432202.jpg',
-      }         
+    }         
     ]
 });

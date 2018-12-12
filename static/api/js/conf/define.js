22780147e53542a56014713956944c3bdef5ddce
WHY666 <root@vultr.guest>
2018-12-12 08:16:12 +0000

    Site updated: 2018-12-12 08:16:11

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,1 +1,3 @@
+// build time:Wed Dec 12 2018 08:00:48 GMT+0000 (Coordinated Universal Time)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
+//rebuild by neat 
---
+++ static/api/js/trans/logger.js
@@ -0,0 +1,3 @@
+// build time:Wed Dec 12 2018 08:00:48 GMT+0000 (Coordinated Universal Time)
+
+//rebuild by neat 

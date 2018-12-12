dd1dcedfda1475e1c77e3e82474281925f4ee4f1
WHY666 <root@vultr.guest>
2018-12-12 08:44:21 +0000

    Site updated: 2018-12-12 08:44:19

--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,1 @@
-// build time:Wed Dec 12 2018 08:00:48 GMT+0000 (Coordinated Universal Time)
-
-//rebuild by neat 
+;
--- static/api/js/trans/logger.js
+++
@@ -1,3 +0,0 @@
-// build time:Wed Dec 12 2018 08:00:48 GMT+0000 (Coordinated Universal Time)
-
-//rebuild by neat 
